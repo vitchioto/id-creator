2798d291929189de3a4557df1c414e968978263d
Vladimír Vladovič <vladovic.vladimir@slsp.sk>
2025-06-27 16:38:31 +0200

    [WIP] Clean up form and add MRZ

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import idFront from '../assets/id_front.jpg';
 import idBack from '../assets/id_back.jpg';
 import { state } from '../store.ts';
@@ -23,7 +23,25 @@ let imageBack = ref();
 watch(state, () => {
   drawFront();
   drawBack();
-})
+});
+
+const mrzLine1 = computed(() => {
+  const birthString = state.birthNumber.replace('/', '');
+
+  return `IDSVK${state.idNumber}<3${birthString}<<<<<`;
+});
+
+const mrzLine2 = computed(() => {
+  const birthStringParts = state.birthNumber.split('/');
+  
+  return `${birthStringParts}X${state.sex}`;
+});
+
+const mrzLine3 = computed(() => {
+  const nameGTs = '<'.repeat(28 - (state.surname.length + state.name.length));
+  
+  return `${state.surname.toUpperCase()}<<${state.name.toUpperCase()}${nameGTs}`;
+});
 
 const downloadFront = () => {
   const createEl = document.createElement('a');
@@ -56,25 +74,25 @@ const drawFront = () => {
   contextFront.value.drawImage(imageFront.value, 0,0, 1080, 700, 0,0, 540, 350);
 
   contextFront.value.font = "400 16px sans-serif";
-  contextFront.value.fillText(state.surname, 226, 95);
+  contextFront.value.fillText(state.surname, 227, 95);
 
-  contextFront.value.fillText(state.name, 226, 125);
+  contextFront.value.fillText(state.name, 227, 125);
 
-  contextFront.value.fillText(state.birthDate, 226, 155);
+  contextFront.value.fillText(state.birthDate, 227, 155);
 
-  contextFront.value.fillText(state.nationality, 226, 185);
+  contextFront.value.fillText(state.nationality, 227, 185);
 
-  contextFront.value.fillText(state.idNumber, 226, 215);
+  contextFront.value.fillText(state.idNumber, 227, 215);
 
-  contextFront.value.fillText(state.issuer, 226, 255);
+  contextFront.value.fillText(state.issuer, 227, 255);
 
-  contextFront.value.fillText(state.sex, 385, 155);
+  contextFront.value.fillText(state.sex, 383, 155);
 
-  contextFront.value.fillText(state.birthNumber, 385, 185);
+  contextFront.value.fillText(state.birthNumber, 383, 185);
 
-  contextFront.value.fillText(state.issueDate, 385, 215);
+  contextFront.value.fillText(state.issueDate, 383, 215);
 
-  contextFront.value.fillText(state.expirationDate, 385, 255);
+  contextFront.value.fillText(state.expirationDate, 383, 255);
 }
 
 const drawBack = () => {
@@ -89,6 +107,14 @@ const drawBack = () => {
   contextBack.value.fillText(state.birthSurname, 245, 86);
 
   contextBack.value.fillText(state.birthPlace, 245, 113);
+
+  contextBack.value.font = "400 20px sans-serif";
+  contextBack.value.letterSpacing = "3px";
+  contextBack.value.fillText(mrzLine1.value, 30, 250);
+
+  contextBack.value.fillText(mrzLine2.value, 30, 280);
+
+  contextBack.value.fillText(mrzLine3.value, 30, 310);
 }
 
 onMounted(() => {
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,58 +1,57 @@
 <template>
-  <h2>Form</h2>
   <form>
     <label class="label" for="surname">
       Priezvisko
     </label>
-    <input id="surname" class="input" v-model="state.surname">
+    <input id="surname" class="input" placeholder="Mrkvička" v-model="state.surname">
     <label class="label" for="name">
       Meno
     </label>
-    <input id="name" class="input" v-model="state.name">
+    <input id="name" class="input" placeholder="Jožko" v-model="state.name">
     <label class="label" for="birthDate">
       Datum narodenia
     </label>
-    <input id="birthDate" class="input" v-model="state.birthDate">
+    <input id="birthDate" class="input" placeholder="Mrkvička" v-model="state.birthDate">
     <label class="label" for="nationality">
       Obcianstvo
     </label>
-    <input id="nationality" class="input" v-model="state.nationality">
+    <input id="nationality" class="input" placeholder="Mrkvička" v-model="state.nationality">
     <label class="label" for="idNumber">
       Cislo preukazu
     </label>
-    <input id="idNumber" class="input" v-model="state.idNumber">
+    <input id="idNumber" class="input" placeholder="Mrkvička" v-model="state.idNumber">
     <label class="label" for="issuer">
       Vydal
     </label>
-    <input id="issuer" class="input" v-model="state.issuer">
+    <input id="issuer" class="input" placeholder="Mrkvička" v-model="state.issuer">
     <label class="label" for="sex">
       Pohlavie
     </label>
-    <input id="sex" class="input" v-model="state.sex">
+    <input id="sex" class="input" placeholder="Mrkvička" v-model="state.sex">
     <label class="label" for="birthNumber">
       Rodne cislo
     </label>
-    <input id="birthNumber" class="input" v-model="state.birthNumber">
+    <input id="birthNumber" class="input" placeholder="Mrkvička" v-model="state.birthNumber">
     <label class="label" for="issueDate">
       Datum vydania
     </label>
-    <input id="issueDate" class="input" v-model="state.issueDate">
+    <input id="issueDate" class="input" placeholder="Mrkvička" v-model="state.issueDate">
     <label class="label" for="expirationDate">
       Datum expiracie
     </label>
-    <input id="expirationDate" class="input" v-model="state.expirationDate">
+    <input id="expirationDate" class="input" placeholder="Mrkvička" v-model="state.expirationDate">
     <label class="label" for="address">
       Adresa
     </label>
-    <input id="address" class="input" v-model="state.address">
+    <input id="address" class="input" placeholder="Mrkvička" v-model="state.address">
     <label class="label" for="birthSurname">
       Rodne priezvisko
     </label>
-    <input id="birthSurname" class="input" v-model="state.birthSurname">
+    <input id="birthSurname" class="input" placeholder="Mrkvička" v-model="state.birthSurname">
     <label class="label" for="birthPlace">
       Rodne mesto
     </label>
-    <input id="birthPlace" class="input" v-model="state.birthPlace">
+    <input id="birthPlace" class="input" placeholder="Mrkvička" v-model="state.birthPlace">
   </form>
 </template>
 
