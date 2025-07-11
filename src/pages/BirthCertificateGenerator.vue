2fbb6d6ec98671ba42a25e0f5642a58e8b95f640
Vladimír Vladovič <vladovic.vladimir@slsp.sk>
2025-07-11 13:55:06 +0200

    Fix eslint issues

--- src/pages/BirthCertificateGenerator.vue
+++ src/pages/BirthCertificateGenerator.vue
@@ -5,9 +5,6 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { state } from '../storeBC.ts';
-
 import Form from '../components/birthCertificateGenerator/Form.vue';
 import Canvas from '../components/birthCertificateGenerator/Canvas.vue';
 
